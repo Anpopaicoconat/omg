632289a587be407ec1e2fc1872ad337e593e4ee1
Anpopaicoconat <43803276+Anpopaicoconat@users.noreply.github.com>
2022-06-16 20:28:43 +0300

    Update models.py

--- models.py
+++ models.py
@@ -220,3 +220,83 @@ class pl_cnn2d(pl.LightningModule):
         self.log('val_f1_epoch', f1)
         self.log('val_acc_epoch', acc)
         print('f1', f1, 'acc', acc)
+        
+class pl_efnet(pl.LightningModule):
+    def __init__(self, le, extractor, main_modul, n_input=1, n_output=41):
+        super().__init__()
+        self.le = le
+        self.extractor = extractor
+
+        
+        self.main_modul = main_modul
+
+        self.out_activation = torch.nn.LogSoftmax()
+
+        #loass & metrics
+        self.f1_train = torchmetrics.F1Score(num_classes=n_output)
+        self.f1_val = torchmetrics.F1Score(num_classes=n_output)
+        self.acc_train = torchmetrics.Accuracy(num_classes=n_output)
+        self.acc_val = torchmetrics.Accuracy(num_classes=n_output)
+        self.loss = torch.nn.CrossEntropyLoss()
+
+        # self.automatic_optimization = False
+
+    def forward(self, x):
+        x = self.extractor(x)
+        x = self.main_modul(x)
+        x = self.out_activation(x)
+        return x
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        scheduler = torch.optim.lr_scheduler.LinearLR(optimizer, start_factor=1, end_factor=0.5,  total_iters=50)
+        return [optimizer], [{"scheduler": scheduler, "interval": "epoch"}]
+
+    def training_step(self, train_batch, batch_idx):
+        x, y = train_batch
+        y_pred = self(x)
+        loss = self.loss(y_pred, y)
+        self.log('train_loss', loss)
+        self.f1_train(y_pred, y)
+        self.acc_train(y_pred, y)
+        return loss
+
+    def lr_scheduler_step(self, scheduler, optimizer_idx, metric):
+        scheduler.step()
+
+    def training_epoch_end(self, outs):
+        sch = self.lr_schedulers()
+        sch.step()
+
+        f1 = self.f1_train.compute()
+        acc = self.acc_train.compute()
+        self.log('train_f1_epoch', f1)
+        self.log('train_acc_epoch', acc)
+        print('train_f1', f1, 'train_acc', acc)
+
+    def validation_step(self, val_batch, batch_idx):
+        x, y = val_batch
+        y_pred = self(x)
+        loss = self.loss(y_pred, y)
+        y_pred = torch.argmax(y_pred, dim=1)
+        self.f1_val(y_pred, y)
+        self.acc_val(y_pred, y)
+        self.log('val_loss', loss)
+
+    def validation_epoch_end(self, outs):
+        f1 = self.f1_val.compute()
+        acc = self.acc_val.compute()
+        self.log('val_f1_epoch', f1)
+        self.log('val_acc_epoch', acc)
+        print('val_f1', f1, 'val_acc', acc)
+
+    def predict_step(self, batch, batch_idx, dataloader_idx=0):
+        x, y = batch
+        y_pred = self(x)
+        y_pred = torch.argmax(y_pred, dim=1)
+        y_pred = self.le.inverse_transform(y_pred.cpu().detach().numpy())
+        return y_pred
+# it is main_modul    
+#efnet7 = torchvision.models.efficientnet_b7(pretrained=True)
+#efnet7.features[0][0] = torch.nn.Conv2d(in_channels, 64, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False) #64
+#efnet7.classifier[1] = torch.nn.Linear(in_features=2560, out_features=n_classes, bias=True) #2560
